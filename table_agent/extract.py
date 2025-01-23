3c1c586aca8729b57aab7c344dc8fd7a2afa6c5b
Vincent Min <39170736+vincentmin@users.noreply.github.com>
2025-01-23 20:58:21 +0000

    start building the graph

--- table_agent/extract.py
+++ table_agent/extract.py
@@ -1,6 +1,8 @@
 from typing import Type
 import pandas as pd
 from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import HumanMessage
+from .graph import get_graph, Messages
 
 type TableInput = pd.DataFrame
 
@@ -13,7 +15,9 @@ def load_default_model() -> BaseChatModel:
     from langchain_openai import ChatOpenAI
     return ChatOpenAI(model_name="gpt-4o-mini")
 
-def extract[T](table: TableInput, output_model: Type[T], llm: BaseChatModel | None = None) -> list[T]:
+
+
+def extract[T](table: TableInput, output_model: Type[T], llm: BaseChatModel | None = None, prompt:str = "Extract data from table") -> tuple[str,list[T]]:
     """Takes in a table and an output model and returns a list of output models
 
     Args:
@@ -21,8 +25,16 @@ def extract[T](table: TableInput, output_model: Type[T], llm: BaseChatModel | No
         output_model (Type[T]): A class that represents the output model
 
     Returns:
-        list[T]: A list of output models
+        tuple[str,list[T]]: A tuple of the response and the list of output models
     """
     if llm is None:
         llm = load_default_model()
-    pass
+    graph = get_graph(llm)
+    messages: Messages = graph.invoke({
+        "messages": [HumanMessage(text=prompt)],
+        "df": table,
+        "output_model": output_model
+    })
+    response = messages[-1]
+    outputs: list[T] = messages[-2].artifact
+    return response, outputs
