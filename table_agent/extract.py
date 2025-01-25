3920a8d1d76a284698cc420e37cc37b6ad4cdbe4
Vincent Min <39170736+vincentmin@users.noreply.github.com>
2025-01-25 21:00:44 +0000

    update docstring for extract

--- table_agent/extract.py
+++ table_agent/extract.py
@@ -33,18 +33,10 @@ Then write code that parses each row of the dataframe into a json with the follo
 {schema}
 
 The script should write the parsed jsons to a file called `output.json`.
-Make sure that `ouput.json` is a list of jsons that comply with the above format.
+Make sure that `output.json` contains a list of jsons that comply with the above format.
 
 Here follow the first 5 lines of the dataframe you need to act on:
 {df}"""
-
-DOCKERFILE = """FROM python:3.12-slim
-RUN pip install -U pip && pip install pandas pyarrow
-
-WORKDIR /workspace
-
-CMD ["sleep", "infinity"]
-"""
 
 
 def extract[T: BaseModel](
@@ -52,21 +44,33 @@ def extract[T: BaseModel](
     output_model: Type[T],
     llm: BaseChatModel | None = None,
     system_prompt: str | None = None,
-    user_prompt: str = "Extract data from table",
-    dockerfile: str = DOCKERFILE,
+    user_prompt: str | None = None,
+    docker_image_tag: str | None = None,
     tools: list[BaseTool] | None = None,
     config: RunnableConfig | None = None,
 ) -> TableOutput[T]:
     """Takes in a table and an output model and returns a list of output models
 
     Args:
-        table (pd.DataFrame): A pandas DataFrame
-        output_model (Type[T]): A pydantic model that the table will be parsed into
-        llm (BaseChatModel, optional): A language model. Defaults to None.
-        system_prompt (str, optional): The system prompt. Defaults to None.
-        user_prompt (str, optional): The user prompt. Defaults to "Extract data from table".
-        config (RunnableConfig, optional): The configuration. Defaults to None.
-        tools (list[BaseTool], optional): Any additional tools to be added to the standard tools. Defaults to None.
+        table (pd.DataFrame): A pandas DataFrame.
+        output_model (Type[T]): A pydantic model that the table will be parsed into.
+        llm (BaseChatModel, optional): A language model with tool calling capabilities.
+            Defaults to None in which case OpenAI gpt-4o-mini is used.
+        system_prompt (str, optional): The system prompt.
+            Defaults to None in which case the default system prompt is used.
+            Base yourself on the default system prompt to create your own.
+            We strongly advise to include the schema and a truncated version of your table.
+        user_prompt (str, optional): The user prompt.
+            Defaults to None in which case "Extract data from table" is used.
+        docker_image_tag (str, optional): The tag of the docker image to run llm-generated
+            scripts in. Defaults to None, which uses a python-3.12 image with pandas and
+            pyarrow installed. Provide a custom image tag if you need additional dependencies.
+        tools (list[BaseTool], optional): The tools that the agent can use to complete its task.
+            Defaults to None, in which case the python_tool is used. If you provide a list
+            of tools, make sure it includes the python_tool or a substitute.
+        config (RunnableConfig, optional): The configuration. Defaults to None. See langchain
+            for documentation. If you run into recursion errors, you may want to
+            set the config to `{"recursion_limit": some_high_int}`.
 
     Returns:
         TableOutput: The model response, the script, and the outputs
